5b91d7d00cce3ddc86ded6089f076cc93da912c8
arya271407 <arya271407@gmail.com>
2025-10-31 09:24:03 +0530

    Fix indentation and error handling in vote.py

--- vote.py
+++ vote.py
@@ -1,9 +1,9 @@
 try:
-  age = int(input("Enter Your Age:"))
-  if age>=18:
-    print("You are eligible to vote.")
-  else:
-    print("You are not eligible to vote.")
+    age = int(input("Enter Your Age:"))
+    if age>=18:
+         print("You are eligible to vote.")
+    else:
+         print("You are not eligible to vote.")
 except ValueError:
-    print("Please enter a valid integer for age.")
+     print("Please enter a valid integer for age.")
     
